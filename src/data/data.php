1f356cd79f3c259fe8f02e0a07fbf96fad1b0243
lrcampos97 <campos.luiz97@gmail.com>
2020-08-18 09:55:13 -0300

    Update methods to getBalance and postEvent, adjusting the programming logic

--- src/data/data.php
+++ src/data/data.php
@@ -6,6 +6,8 @@ require_once('../src/classes/Account.php');
 const SESSION_ACCOUNT = "ACCOUNT";
 const RETURN_RESET = "200 OK";
 const RETURN_NOT_FOUND = "404 0";
+const RETURN_EXISTING_ACCOUNT = "200 ";
+const RETURN_DEPOSIT = "201 ";
 
 
     function resetSession(){
@@ -16,13 +18,10 @@ const RETURN_NOT_FOUND = "404 0";
         return RETURN_RESET;
     }
 
-    function addAccount($id, $balance){
-        
-        verifySession();
-
+    function addAccount($id, $balance){            
         $account = new Account($id, $balance);
 
-        array_push($_SESSION[SESSION_ACCOUNT], $account);
+        array_push($_SESSION[SESSION_ACCOUNT], $account->getObjectJSON());
        
         return prepareReturn($account->getObjectJSON());
     }
@@ -31,11 +30,55 @@ const RETURN_NOT_FOUND = "404 0";
 
         $arr = array('destination'=>$object);
 
-        return "201 " . json_encode($arr);
+        return RETURN_DEPOSIT . json_encode($arr);
     }
 
-    function verifySession(){
+    function existingDestination($id){        
+        $recordKey = -1;
 
+        foreach ($_SESSION[SESSION_ACCOUNT] as $key=>$value) {
+
+            $account = json_decode($value);
+                        
+            if($account->id === $id){
+                $recordKey = $key;                   
+                break;
+            }
+
+        }
+
+        return $recordKey;
+    }
+
+    function updateAccount($id, $amount, $arrayKey){             
+
+        $account = json_decode($_SESSION[SESSION_ACCOUNT][$arrayKey]);
+
+        $newAccount = new Account($id, $account->balance);
+        $newAccount->setbalance($amount);
+        
+        unset($_SESSION[SESSION_ACCOUNT][$arrayKey]);
+
+        return addAccount($newAccount->getid(),$newAccount->getbalance());         
+    }
+    
+    function getBalanceFromAccount($id){
+        
+        $arrayKey = existingDestination($id);
+
+        if ($arrayKey !== -1){
+
+            $account = json_decode($_SESSION[SESSION_ACCOUNT][$arrayKey]);
+                          
+            return RETURN_EXISTING_ACCOUNT . $account->balance;
+
+        } else {
+            return RETURN_NOT_FOUND;
+        }
+    }
+
+
+    function verifySession(){
         if(!isset($_SESSION[SESSION_ACCOUNT])){
             resetSession();
         }
--- src/routes/bank.php
+++ src/routes/bank.php
@@ -18,17 +18,41 @@ $app->post('/reset', function(Request $request, Response $response){
 });
 
 
+$app->get('/balance', function(Request $request, Response $response){        
+
+    
+    if(isset($_GET['account_id'])){
+
+        return getBalanceFromAccount($_GET['account_id']);
+        
+    }
+        
+});
+
+
 $app->post('/event', function(Request $request, Response $response, $args){
     $body = $request->getBody();
 
     $values = json_decode($body, true);
 
     if (isset($values["type"]) && $values["type"] !== ""){
+        
+        verifySession();
 
         switch ($values["type"]) {
             case "deposit":
-                
-                return addAccount($values["destination"], $values["amount"]);                
+
+                 $arrayKey = existingDestination($values["destination"]);
+
+                 if ($arrayKey !== -1){
+                    
+                   return updateAccount($values["destination"], $values["amount"], $arrayKey);      
+                                      
+                 } else {
+                    
+                    return addAccount($values["destination"], $values["amount"]);    
+
+                 }               
 
                 break;
             default:
